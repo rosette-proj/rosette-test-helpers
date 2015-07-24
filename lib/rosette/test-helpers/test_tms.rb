18d698715e5c78c8b276b4673d2cea84ec532eb8
Cameron Dutro <camertron@gmail.com>
2015-07-24 13:56:20 -0700

    Remove granularity from TestTms#Repository
    It just feels gross

--- lib/rosette/test-helpers/test_tms.rb
+++ lib/rosette/test-helpers/test_tms.rb
@@ -34,11 +34,11 @@ module Rosette
           clear
         end
 
-        def store_phrases(phrases, commit_id, granularity = PhraseStorageGranularity::COMMIT)
+        def store_phrases(phrases, commit_id)
           stored_phrases[commit_id] += phrases
         end
 
-        def store_phrase(phrase, commit_id, granularity = PhraseStorageGranularity::COMMIT)
+        def store_phrase(phrase, commit_id)
           stored_phrases[commit_id] << phrase
         end
 
